d9afac91a8ce37995e731350fb1f8479415e01ab
Angel <kayron013@gmail.com>
2021-01-25 15:05:20 -0500

    (refac)

--- src/web-server/routes/meetings/createMeeting.ts
+++ src/web-server/routes/meetings/createMeeting.ts
@@ -1,23 +1,22 @@
 import { addHours, addMinutes } from 'date-fns';
-import { createEvent, findFirstFree } from '../../../utils/calendar';
-import { zoomToRFCRecurrence } from '../../../utils/calendar/recurrence';
-import { getZoomAccounts, storeEvent } from '../../../utils/db';
-import { scheduleMeeting, ZoomerMeetingRequest } from '../../../utils/zoom';
+import * as Calendar from '../../../utils/calendar';
+import * as DB from '../../../utils/db';
+import * as Zoom from '../../../utils/zoom';
 import { HandlerResponse } from '../types';
 
-export const createMeeting = async (meetingReq: ZoomerMeetingRequest): Promise<HandlerResponse> => {
+export const createMeeting = async (meetingReq: Zoom.ZoomerMeetingRequest): Promise<HandlerResponse> => {
   const startDT = meetingReq.start_time;
   const endDT = addMinutes(new Date(startDT), meetingReq.duration).toISOString();
-  const accounts = await getZoomAccounts();
+  const accounts = await DB.getZoomAccounts();
 
   const hourBefore = addHours(new Date(startDT), -1).toISOString();
   const hourAfter = addHours(new Date(startDT), 1).toISOString();
 
-  const freeIdx = await findFirstFree(hourBefore, hourAfter, accounts);
+  const freeIdx = await Calendar.findFirstFree(hourBefore, hourAfter, accounts);
   const account = accounts[freeIdx];
 
   if (account) {
-    const meeting = await scheduleMeeting(account.email, meetingReq);
+    const meeting = await Zoom.scheduleMeeting(account.email, meetingReq);
 
     const eventDesc = `${meetingReq.agenda}\n\n----------------------\nScheduled on ${account.email}`;
 
@@ -26,14 +25,14 @@ export const createMeeting = async (meetingReq: ZoomerMeetingRequest): Promise<H
       description: eventDesc,
       start: startDT,
       end: endDT,
-      recurrence: meetingReq.recurrence ? zoomToRFCRecurrence(meetingReq.recurrence) : undefined,
+      recurrence: meetingReq.recurrence ? Calendar.zoomToRFCRecurrence(meetingReq.recurrence) : undefined,
     };
 
-    const zoomCalEventID = await createEvent(account.calendarID, eventReq);
+    const zoomCalEventID = await Calendar.createEvent(account.calendarID, eventReq);
     //const [leaderCalErr, leaderCalEventID] = await createEvent(freeCal, eventReq);
     const leaderCalEventID = '~' + Math.random();
 
-    await storeEvent({
+    await DB.storeEvent({
       zoomAccount: account.email,
       title: meetingReq.topic,
       description: meetingReq.agenda,
--- src/web-server/routes/meetings/deleteMeeting.ts
+++ src/web-server/routes/meetings/deleteMeeting.ts
@@ -1,23 +1,23 @@
-import { deleteEvent } from '../../../utils/calendar';
-import { getEvent, getZoomAccount, removeEvent } from '../../../utils/db';
-import { cancelMeeting } from '../../../utils/zoom/requests';
+import * as Calendar from '../../../utils/calendar';
+import * as DB from '../../../utils/db';
+import * as Zoom from '../../../utils/zoom';
 import { HandlerResponse } from '../types';
 
 export const deleteMeeting = async (meetingID: string): Promise<HandlerResponse> => {
-  const dbMeeting = await getEvent(meetingID);
+  const dbMeeting = await DB.getEvent(meetingID);
   if (!dbMeeting) {
     return { success: false, error: 'meeting not found' };
   }
 
-  const zoomAccount = await getZoomAccount(dbMeeting.zoomAccount);
+  const zoomAccount = await DB.getZoomAccount(dbMeeting.zoomAccount);
   if (!zoomAccount) {
     return { success: false, error: 'zoom account not found' };
   }
 
-  await cancelMeeting(meetingID);
-  await deleteEvent(zoomAccount.calendarID, dbMeeting.calendarEvents.zoomEventID);
-  //await deleteEvent(zoomAccount.calendarID, dbMeeting.calendarEvents.leadershipEventID);
-  await removeEvent(meetingID);
+  await Zoom.cancelMeeting(meetingID);
+  await Calendar.deleteEvent(zoomAccount.calendarID, dbMeeting.calendarEvents.zoomEventID);
+  //await Calendar.deleteEvent(zoomAccount.calendarID, dbMeeting.calendarEvents.leadershipEventID);
+  await DB.removeEvent(meetingID);
 
   return { success: true, data: null };
 };
