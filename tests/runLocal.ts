49ffd50d7ac6922401d5b77733c6a84ca663cb7f
Angel <kayron013@gmail.com>
2021-02-19 03:07:54 -0500

    (fix) initgapis

--- tests/runLocal.ts
+++ tests/runLocal.ts
@@ -1,10 +1,8 @@
 import 'dotenv/config';
 import { Server } from 'http';
 import { app } from '../src/handlers';
-import { initGAPIs } from '../src/services/googleapis';
 
 const runLocalServer = async () => {
-  await initGAPIs();
   const server = new Server(app);
   const PORT = 8000;
 
